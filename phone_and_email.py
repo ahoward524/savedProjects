dd98246290cdb8d35a3ad781719f0fb83423162e
ahoward524 <ahoward524@gmail.com>
2018-01-12 11:46:55 -0600

    Added phone_and_email.py

---
+++ phone_and_email.py
@@ -0,0 +1,46 @@
+# ! python3
+
+import re, pyperclip
+
+
+
+# Create a regex for phone numbers
+
+phoneRegex = re.compile(r'''
+
+(
+((\d\d\d) | (\(\d\d\d\)))?    # area code (optional)
+(\s | -)                      # first separator
+\d\d\d                        # first 3 digits
+-                             # separator
+\d\d\d\d                      # last 4 digits
+(((ext (\.)?\s) |x)           # extension word part (optional)
+ (\d{2,5}))?
+)                   # extension number part
+''',re.VERBOSE)
+
+# Create a regex for email addresses
+emailRegex = re.compile(r'''
+[a-zA-Z0-9_.+]+        # name part
+@                      # @ symbol
+[a-zA-Z0-9_.+]+        # domain name 
+''',re.VERBOSE)
+
+# Get the text off the clipboard
+text = pyperclip.paste()
+
+
+
+
+# Extract the email/phone from this text
+extractedPhone = phoneRegex.findall(text)
+extractedEmail = emailRegex.findall(text)
+
+allPhoneNumbers = []
+for phoneNumber in extractedPhone:
+    allPhoneNumbers.append(phoneNumber[0])
+
+# Copy the extracted email/phone to the clipboard
+results = '\n'.join(allPhoneNumbers) + '\n' + '\n'.join(extractedEmail)
+pyperclip.copy(results)
+
